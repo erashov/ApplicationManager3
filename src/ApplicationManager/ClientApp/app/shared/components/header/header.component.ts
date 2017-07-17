7529af9bce4e036f254ed3b103bc9fe948579ace
Alex <erashov@outlook.com>
2017-07-17 17:46:55 +0300

    update

--- src/ApplicationManager/ClientApp/app/shared/components/header/header.component.ts
+++ src/ApplicationManager/ClientApp/app/shared/components/header/header.component.ts
@@ -23,13 +23,4 @@ export class HeaderComponent implements OnInit {
         const dom: any = document.querySelector('body');
         dom.classList.toggle('push-right');
     }
-
-    rltAndLtr() {
-        const dom: any = document.querySelector('body');
-        dom.classList.toggle('rtl');
-    }
-
-    onLoggedout() {
-        localStorage.removeItem('isLoggedin');
-    }
 }
